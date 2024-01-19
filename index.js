bd1d53ab05bd19d9f4356f18b4e3f06c01f4a0dc
Jeet Desai <jeetdesai2004@gmail.com>
2024-01-20 00:39:13 +0530

    added material section

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ for(let i=0;i<btns.length;i++)
         document.getElementById(this.dataset.id).classList.add("active");
     });
 }
+
+const materialItems=document.querySelectorAll(".material-item");
+
+materialItems[0].addEventListener("click",()=>window.open("https://drive.google.com/drive/folders/1vPdELOVoNSym_2WZpehWJJ6QYClO_77c"));
+materialItems[1].addEventListener("click",()=>window.open("https://drive.google.com/drive/folders/1U0XqPr-CV-XdQbuB23NggwOAUTbDsyG3"));
+materialItems[2].addEventListener("click",()=>window.open("https://drive.google.com/drive/folders/1me_H18v5kmKVe60vp3e51oiRc3_qRmRi"));
+materialItems[3].addEventListener("click",()=>window.open("https://drive.google.com/drive/folders/1cMTEnjsGLDC0__Di6qH-wZYjN7shxnoi"));
+
